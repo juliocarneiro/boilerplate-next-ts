26a4cddc0b0a7dacc3f54105694db9642fa2b9a9
juliocarneiro <juliocarneiro@gmail.com>
2022-03-10 17:26:09 -0300

    Config aws and actions

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -5,7 +5,7 @@ import Home from 'pages/index'
 describe('HomePage', () => {
   it('should render the index page', () => {
     render(<Home />)
-    const heading = screen.getByText(/Boilerplate ::/i)
+    const heading = screen.getByText(/Boilerplate/i)
     expect(heading).toBeInTheDocument()
   })
 })
