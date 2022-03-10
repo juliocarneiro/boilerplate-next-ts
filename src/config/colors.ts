4330c67914aa1ec22f114d8e7ff97b298836f5f1
juliocarneiro <juliocarneiro@gmail.com>
2022-03-10 16:14:54 -0300

    test

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -2,7 +2,6 @@ export const COLORS = {
   primary: '#F5F5F5',
   secondary: '#FF5500',
   tertiary: '#4A4A4A',
-  quaternary: '#E412FF',
   quinary: '#00B99C',
   white: '#ffffff',
   black: '#000000'
