d8cefbfd67b29ca68680b74c0fec884533f3f6f3
juliocarneiro <juliocarneiro@gmail.com>
2022-03-09 22:23:36 -0300

    tests

--- src/tests/404.test.tsx
+++ src/tests/404.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '../test-utils'
+import { render, screen } from './test-utils'
 import NotFound from 'pages/404'
 
 describe('HomePage', () => {
