3efcc6f779f1b8afa16db7a6e5ef319c6c4c3f2c
Júlio Carneiro <juliocarneiro@gmail.com>
2022-10-20 19:13:58 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
-    removeConsole: {
-      exclude: ['error']
-    },
     styledComponents: true
   },
   images: {
