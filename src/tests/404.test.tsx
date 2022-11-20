c3606e049775969596c541a544372b86cb537389
juliocarneiro <juliocarneiro@gmail.com>
2022-11-20 19:22:47 -0300

    ajust dark mode tests

--- src/tests/404.test.tsx
+++ src/tests/404.test.tsx
@@ -1,9 +1,15 @@
 import { screen, render } from '@testing-library/react'
+import { lightTheme } from 'config'
 import NotFound from 'pages/404'
+import { ThemeProvider } from 'styled-components'
 
 describe('HomePage', () => {
   it('should render the 404 page', () => {
-    render(<NotFound />)
+    render(
+      <ThemeProvider theme={lightTheme}>
+        <NotFound />
+      </ThemeProvider>
+    )
     const heading = screen.getByText(/Página não encontrada./i)
     expect(heading).toBeInTheDocument()
   })
--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -1,9 +1,15 @@
 import { screen, render } from '@testing-library/react'
+import { lightTheme } from 'config'
 import Home from 'pages/index'
+import { ThemeProvider } from 'styled-components'
 
 describe('HomePage', () => {
   it('should render the index page', () => {
-    render(<Home />)
+    render(
+      <ThemeProvider theme={lightTheme}>
+        <Home />
+      </ThemeProvider>
+    )
     const heading = screen.getByText(/Boilerplate/i)
     expect(heading).toBeInTheDocument()
   })
